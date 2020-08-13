d428b70f69602f28fa3af4876623ff4c202b83dd
LikithaVenna <68274754+LikithaVenna@users.noreply.github.com>
2020-08-13 14:10:34 +0530

    Create Class Lib file for Calculator

---
+++ Calulator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAssignment
+{
+    public class Calulator
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public int Add()
+        {
+            return num1 + num2;
+        }
+        public int Subtract()
+        {
+            int res;
+            if (num1 > num2)
+            {
+                res= num1 - num2;
+            }
+            else
+            {
+                res = num2 - num1;
+            }
+            return res;
+        }
+        public int Multiply()
+        {
+            return num1 * num2;
+        }
+        public double Divide()
+        {
+            int res;
+            if(num1>num2)
+            {
+                res = num1 / num2;
+            }
+            else
+            {
+                res = num2 / num1;
+            }
+            return res;
+        }
+    }
+}
